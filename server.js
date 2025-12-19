7cf112382c01a9c3dc714bbf7337a1e85ef3f618
shiv788111 <Shiv.k@foundercodes.com>
2025-12-19 16:50:38 +0530

    Add Swagger UI for API documentation

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
 import prisma from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import agencyRoutes from "./routes/agencyRoutes.js";
+import requestRoutes from "./routes/requestRoutes.js";
+import vehicleRoutes from "./routes/vehicleRoutes.js";
 
 dotenv.config();
 
@@ -15,16 +19,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// uploaded images
+// Static uploads
 app.use("/uploads", express.static("uploads"));
 
-// Swagger UI
+// Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-
-//api
-
-
+// APIs
+app.use("/api/user", userRoutes);
+app.use("/api/agency", agencyRoutes);
+app.use("/api/request", requestRoutes);
+app.use("/api/vehicle", vehicleRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
